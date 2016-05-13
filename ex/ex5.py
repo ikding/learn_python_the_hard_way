3d1ce1f79766d795f484b00534219284a80a6c59
I-Kang Ding <ikang.ding@capitalone.com>
2016-05-13 13:52:32 -0400

    Ex5: add unit conversion calculations

--- ex/ex5.py
+++ ex/ex5.py
@@ -5,10 +5,11 @@ weight = 180
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
+bool_var = True
 
 print "Let's talk about %s." % name
-print "He's %d inches tall." % height
-print "He's %d pounds heavy." % weight
+print "He's %d inches (%d centimeters) tall." % (height, height*2.54)
+print "He's %d pounds (%d kilograms) heavy." % (weight, weight/2.2)
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
@@ -16,3 +17,5 @@ print "His teeth are usually %s depending on the coffee." % teeth
 # This line is tricky, try to get it exactly right
 print "If I add %d, %d, and %d I get %d." % (
     age, height, weight, age + height + weight)
+
+print "The boolean variable is: %r" % bool_var
